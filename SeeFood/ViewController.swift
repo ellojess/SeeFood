d41c1aff793e02f636cba9210c9c4342022e9918
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-05-31 16:58:52 -0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Bo on 5/31/19.
+//  Copyright © 2019 Jessica Trinh. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
