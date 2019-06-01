a2524e1cebf65c75b15598687f50689ce12b2359
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-06-01 00:26:17 -0700

    Getting image recognition results back from V3 Model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -29,10 +29,55 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("loading CoreML Model Failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = result.first{
+                
+                //Silicon Valley Version: Hotdog or Not Hotdog
+//                if firstResult.identifier.contains("hotdog"){
+//                    self.navigationItem.title = "Hotdog!"
+//                } else {
+//                    self.navigationItem.title = "Not Hotdog!"
+//                }
+                
+                //My version: Actually recognize item in photo
+                
+                let parts = firstResult.identifier.components(separatedBy: ",")
+                print(parts[0])
+                self.navigationItem.title = parts[0]
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
